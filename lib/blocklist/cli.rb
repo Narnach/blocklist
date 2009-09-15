ebfcb34841339e8c737b9a867fcc4acbf30c9142
Wes Oldenbeuving <narnach@gmail.com>
2009-09-15 15:07:13 +0200

    Moved contents of bin/blocklist to Blacklist::Cli
    This helps to keep things DRY and allows for testing

---
+++ lib/blocklist/cli.rb
@@ -0,0 +1,102 @@
+require 'blocklist'
+
+class Blocklist
+  class Cli
+    def initialize(argv)
+      @argv = argv
+      @bl = Blocklist.new
+      @bl.parse(File.read('/etc/hosts'))
+      @dry_run = !@argv.delete('-d').nil?
+      @quiet = !@argv.delete('-q').nil?
+      @command = @argv.shift || 'help'
+    end
+
+    def run
+      if %w[add list toggle help].include?(@command)
+        self.send(@command)
+      else
+        help "Command unknown: '#{@command}'"
+      end
+      nil
+    end
+    
+    protected
+    
+    def list
+      puts @bl.blocks.map {|block| block.name}.join("\n")
+    end
+    
+    def add
+      block_name = @argv.shift
+      block = @bl.block(block_name) || Blocklist::Block.new(block_name)
+
+      domains = block.lines.map {|line| line.domains}.flatten
+      saved_domains = @argv.map do |domain|
+        dom_segments = domain.split(".")
+        tld_size = 1
+        tld_size = 2 if %w[uk].include? dom_segments.last
+        tld = dom_segments[-tld_size..-1].join(".")
+        dom_no_tld = dom_segments[0...-tld_size]
+        domain_base = dom_no_tld.last
+        subdomain = dom_no_tld.size == 1 ? nil : dom_no_tld[0...-1].join(".")
+        new_domains = [nil, 'www', subdomain].uniq.map {|sub| [sub, domain_base, tld].compact.join(".")} - domains
+        if new_domains.size > 0
+          block.lines << Blocklist::Line.new('127.0.0.1', *new_domains)
+          domains.concat(new_domains)
+        end
+        new_domains
+      end
+      display_result
+      save if saved_domains.flatten.size > 0
+    end
+    
+    def toggle
+      block_name = @argv.shift
+      unless block = @bl.block(block_name)
+        help "Could not toggle non-existing block '#{block_name}'"
+      end
+      block.toggle_comments
+      display_result
+      save
+    end
+    
+    def help(msg=nil)
+      if msg
+        $stderr.puts(msg)
+        $stderr.puts('')
+      end
+      $stderr.puts <<-STR
+Syntax: #{$0} [flags] <command>
+
+Flags:
+  -d
+    Perform a dry-run. Does not modify /etc/hosts
+  -q
+    Quiet mode. Minimizes the output to STDOUT
+
+Commands:
+  add <block name> [domain1] .. [domainN]
+    Add a number of domains to the specified block.
+    Each domain will automatically be added with the www subdomain and without subdomain.
+    Duplicate domains are skipped.
+  list
+    Shows a list of all blocks currently defined
+  toggle <block name>
+    Toggle the comment status of all lines in this block.
+  help
+    Show this page
+  STR
+      exit 1
+    end
+    
+    private
+    
+    def display_result
+      puts @bl unless @quiet
+    end
+    
+    def save
+      File.open('/etc/hosts','w') {|f| f.puts(@bl.to_s)} unless @dry_run
+    end
+  end
+end
---
+++ spec/blocklist/cli_spec.rb
@@ -0,0 +1,6 @@
+require File.join(File.dirname(__FILE__), %w[.. spec_helper])
+require 'blocklist/cli'
+
+describe Blocklist::Cli do
+  it 'should be tested'
+end
